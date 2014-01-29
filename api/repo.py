ffa608b4d2bd568a458fa2b739f5094d73480da0
Graham McGregor <haxxor42@gmail.com>
2014-01-29 00:45:05 -0500

    update & commit files with dulwich

--- api/repo.py
+++ api/repo.py
@@ -10,6 +10,7 @@ from api import config, ConfigException
 from dulwich.repo import Repo
 from dulwich.client import HttpGitClient
 from dulwich import index
+import yaml
 
 
 class NotEmptyRepoError(IOError):
@@ -39,6 +40,7 @@ def clone(bare=False):
 
 
 def pull_remote():
+    repo_dir = config['DATA_LOCAL']
     repo = Repo(repo_dir)
     client = HttpGitClient(os.path.dirname(config['DATA_REMOTE']))
     remote_refs = client.fetch(os.path.basename(config['DATA_REMOTE']), repo)
@@ -46,3 +48,30 @@ def pull_remote():
     indexfile = repo.index_path()
     tree = repo["HEAD"].tree
     index.build_index_from_tree(repo.path, indexfile, repo.object_store, tree)
+
+
+def update_file(file_path, new_data):
+    if not isinstance(new_data, dict):
+        raise ValueError
+
+    repo_dir = config['DATA_LOCAL']
+    repo = Repo(repo_dir)
+
+    abs_path = os.path.join(repo_dir, file_path)
+    with open(abs_path, 'r') as infile:
+        raw_data = yaml.load(infile)
+
+    for key in new_data:
+        raw_data[key] = new_data[key]
+
+    with open(abs_path, 'w') as outfile:
+        outfile.write(yaml.dump(raw_data))
+
+    repo.stage([file_path])
+
+
+def commit_changes(message=''):
+    repo_dir = config['DATA_LOCAL']
+    repo = Repo(repo_dir)
+    c_id = repo.do_commit(message=message, committer='qcumber api <api@qcumber.ca>', ref='refs/heads/master')
+    repo.refs['refs/heads/master'] = c_id
